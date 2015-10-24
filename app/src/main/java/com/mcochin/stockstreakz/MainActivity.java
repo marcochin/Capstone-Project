c05cc8cb87016ff55f202fc32af606e5cbdb97fc
marcochin <mco6055@gmail.com>
2015-10-23 19:31:00 -0700

    deleted v21 styles, added stock up/down drawables, started on searchView

--- app/src/main/java/com/mcochin/stockstreakz/MainActivity.java
+++ app/src/main/java/com/mcochin/stockstreakz/MainActivity.java
@@ -1,10 +1,13 @@
 package com.mcochin.stockstreakz;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -35,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setQueryHint(getString(R.string.action_search_hint));
+        //MenuItemCompat.expandActionView();
         return true;
     }
 
